c55f99a5c2ada15c5187c3b0bf2ee821891029fb
Shubham Singh <mynameshubham23@gmail.com>
2024-05-15 15:12:13 +0530

    Eye detction using faceapi.js

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ Promise.all([
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  navigator.getUserMedia({ video: {} }, stream => video.srcObject = stream, err => console.error(err))
 }
 
 video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-  }, 100)
+  detectScreenAttention(video)
 })
+
+async function detectScreenAttention(videoElement) {
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(videoElement, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    detections.forEach(detection => {
+        let leftEye = detection.landmarks.getLeftEye()
+        let rightEye = detection.landmarks.getRightEye()
+
+        console.log('Eyes detected')
+    });
+    if (!detections.length) {
+        console.log('No eyes detected')
+    }
+}, 500);
+}
